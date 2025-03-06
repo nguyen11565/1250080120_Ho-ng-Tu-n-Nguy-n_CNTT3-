8fb585d151275ebb333418ab1bd612b4d10cef2e
nguyen11565 <hoangtuannguyen1511@gmail.com>
2025-03-06 18:46:29 +0700

    Add files via upload

---
+++ hinhhoc.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package baitaptuan7;
+public class hinhhoc {
+    public final float PI=3.14f;
+    public String ten;
+    public float chuvi;
+    public float dientich;
+    public float thetich;
+    public hinhhoc(){
+        ten="hinh hoc";
+}
+    public void xuatten(){
+     System.out.println("xuat ten"+ten);
+    }
+    public void tinhchuvi(){
+        System.out.println("tinh chu vi"+chuvi);
+    }
+    public void tinhdientich(){
+        System.out.println("tinh dien tich"+dientich);
+    }
+    public void tinhthetich(){
+        System.out.println("tinh the tich"+thetich);
+}
+}
+
+}
